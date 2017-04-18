dc0ce608ba4e9348a2618ece8443a8bdf20f7af1
Mahesh-MSFT <maksh@microsoft.com>
2017-04-18 15:48:56 +0530

    ML Response Working

---
+++ src/PowerBIDemo/Models/MLAPIResponse.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PowerBIDemo.Models
+{
+    public class MLAPIResponse
+    {
+        public string AddedOn { get; set; }
+        public string ConnectionID { get; set; }
+        public string IP { get; set; }
+        public string CartItem { get; set; }
+        public string Scored_Probabilities_for_Class_Books { get; set; }	
+        public string Scored_Probabilities_for_Class_Clothes { get; set; }
+        public string Scored_Probabilities_for_Class_Electronics { get; set; }
+        public string Scored_Probabilities_for_Class_Flowers { get; set; }
+        public string Scored_Probabilities_for_Class_Food { get; set; }
+        public string Scored_Probabilities_for_Class_Furniture { get; set; }
+        public string Scored_Probabilities_for_Class_Perfumes { get; set; }
+        public string Scored_Probabilities_for_Class_Shoes { get; set; }
+        public string Scored_Probabilities_for_Class_Toys { get; set; }
+        public string Scored_Probabilities_for_Class_Wines { get; set; }
+        public string Scored_Labels { get; set; }
+
+        public string Scored_Confidence {
+            get
+            {
+                var tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Books);
+
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Clothes) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Clothes);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Electronics) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Electronics);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Flowers) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Flowers);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Food) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Food);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Furniture) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Furniture);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Perfumes) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Perfumes);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Shoes) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Shoes);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Toys) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Toys);
+                }
+                if (Convert.ToDecimal(Scored_Probabilities_for_Class_Wines) > tempConfidence)
+                {
+                    tempConfidence = Convert.ToDecimal(Scored_Probabilities_for_Class_Wines);
+                }
+
+                return (tempConfidence * 100).ToString("0.00");
+            }
+        }
+
+    }
+}
--- src/PowerBIDemo/Models/OutgoingResponse.cs
+++ src/PowerBIDemo/Models/OutgoingResponse.cs
@@ -9,6 +9,10 @@ namespace PowerBIDemo.Models
     {
         public string NewCartItem { get; set; }
 
+        public string NextPrediction { get; set; }
+
+        public string NextPredictionConfidence { get; set; }
+
         public Dictionary<string, string> CartItemsOfOthers { get; set; }
     }
 }
